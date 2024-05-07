29977993d681c35e3137844adfb8a90c341a1f75
gautam <gautamrathicode@gmail.com>
2024-05-07 06:51:29 +0530

    base project

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const cors = require("cors");
 app.use(express.json()); // Use built-in JSON parser
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    // origin:["http://localhost:3000"],
+    origin:["https://shop-shop-web.netlify.app/"],
     methods:["GET","POST","DELETE"],
     credentials:true
 }));
